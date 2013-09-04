331f334acbbe2b0871dcbbd4ac7eb115689ae2f6
bahelms <jimbonkgit@gmail.com>
2013-09-04 07:13:09 -0400

    adds a cart with line items

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 This is the e-commerce app from 
 [Agile Web Development with Rails 4](http://pragprog.com/book/rails4/agile-web-development-with-rails-4).
+
 It's a product catalog with a shopping cart.
+
+Things to do
+----
+
+* Finish tests
+* Clean up products controller
+* Clean up carts, lineitems controller/views
+* Refactor views into partials
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+
   validates :description, :image_url, presence: true
   validates :title, presence: true, uniqueness: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
@@ -7,7 +9,20 @@ class Product < ActiveRecord::Base
                         message: 'Valid images are .gif, .jpg, .png' 
   }
 
+  before_destroy :ensure_not_referenced_by_line_items
+
   def self.latest
     Product.order(:updated_at).last
   end
+
+  private
+    # Ensure this product isn't a part of existing line items
+    def ensure_not_referenced_by_line_items
+      if line_items.empty?
+        true
+      else
+        errors.add(:base, 'Line Items Present')
+        false
+      end
+    end
 end
