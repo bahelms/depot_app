1713db63139b96ac35d9a6c89dcdaa2a4c1a7580
Jimbonk <jimbonkgit@gmail.com>
2013-10-04 07:04:37 -0400

    adds admin authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Depot::Application.routes.draw do
   root 'store#index', as: 'store'
+  get "admin" => "admin#index"
+  controller :sessions do
+    get "login" => :new
+    post "login" => :create
+    delete "logout" => :destroy
+  end
   resources :line_items, only: [:create, :update, :destroy]
-  resources :carts, only: [:show, :create, :destroy]
+  resources :carts
   resources :products do
     get :who_bought, on: :member
   end
-  resources :orders
+  resources :orders, only: [:new, :create]
   resources :users
 end
